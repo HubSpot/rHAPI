adb3a040a706846207758a6fbba9f5bb9faddeba
Jeremy Hough <jeremyh@clearpassage.com>
2013-04-03 17:30:29 +0000

    fix syntax error

--- lib/r_hapi/connection.rb
+++ lib/r_hapi/connection.rb
@@ -26,6 +26,7 @@ module RHapi
         if RHapi.options[:access_token].nil? or api.eql? 'leads'
           if RHapi.options[:api_key].nil?
             raise(RHapi::UriError, "Call to legacy api requires api key.") unless RHapi.options[:access_token].nil?
+          end
           url << "?hapikey=#{RHapi.options[:api_key]}"
         else
           url << "?access_token=#{RHapi.options[:access_token]}" # not all hubspot APIs support oAuth token calls 
